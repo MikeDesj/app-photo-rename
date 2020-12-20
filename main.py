5f5fbf73f40400bd61d5c2ded59bb24ec4dfff61
Michael <1982mde@gmail.com>
2020-12-20 16:25:17 -0500

    Modif to show the window created with Qt Creator

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 import sys
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtCore import QFile, QIODevice
+from mainwindow import Ui_MainWindow
+
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    ui_file_name = "mainwindow.ui"
-    ui_file = QFile(ui_file_name)
-    if not ui_file.open(QIODevice.ReadOnly):
-        print("Cannot open {}: {}".format(ui_file_name, ui_file.errorString()))
-        sys.exit(-1)
-    loader = QUiLoader()
-    window = loader.load(ui_file)
-    ui_file.close()
-    if not window:
-        print(loader.errorString())
-        sys.exit(-1)
+    window = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(window)
+
     window.show()
 
     sys.exit(app.exec_())
---
+++ mainwindow.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mainwindow.ui'
+##
+## Created by: Qt User Interface Compiler version 6.0.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(50, 50, 191, 32))
+
+        self.pushButton_2 = QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(50, 110, 191, 32))
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 24))
+
+        self.menuApplication_pour_renommer_les_photos = QMenu(self.menubar)
+        self.menuApplication_pour_renommer_les_photos.setObjectName(u"menuApplication_pour_renommer_les_photos")
+        
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuApplication_pour_renommer_les_photos.menuAction())
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"S\u00e9lectionner le dossier", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Renommer les photos", None))
+        self.menuApplication_pour_renommer_les_photos.setTitle(QCoreApplication.translate("MainWindow", u"Application pour renommer les photos", None))
+    # retranslateUi
+
